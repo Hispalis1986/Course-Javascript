7157d3fd71e6f22e7c1e204fc05afce6db1750d6
sirhispalis1986@gmail.com <sirhispalis1986@gmail.com>
2022-09-24 18:11:46 +0200

    Update 24/09/22

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -182,6 +182,74 @@ if(BMIMark > BMIJohn) {
 } else {
     console.log(`John is BMI (${BMIJohn}) is higher than Mark`);
 }
-*/
+
 
 // 20. Type Conversion and Coercion
+// Type conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+console.log(Number('Juanka'));
+console.log(typeof NaN);
+console.log(String(23), 23);
+
+// type coercion
+console.log('I am ' + 23 + ' years old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+
+let n =  '1' + 1;
+n = n - 1;
+console.log(n)
+
+
+// 21.Truthy and Falsy Values
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Juanka'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+const money = 0;
+if(money) {
+    console.log("dont spend it all ;");
+}else {
+    console.log("You should get a job!");
+}
+
+let height = 0;
+if (height) {
+    console.log('YaY! Height is defined!')
+} else {
+    console.log('Height is Undefined');
+}
+
+
+// 22. Equiality Operator: == vs ===
+const age = 18;
+if(age === 18) console.log('You just became an adult :D! (strict)');
+
+
+if(age == 18) console.log('You just became an adult :D! (loose)');
+
+const favourite = Number(prompt("What is your favourite number?"))
+console.log(favourite)
+console.log(typeof favourite)
+
+if (favourite === 23) {
+    console.log('Cool! 23 is an amazing number!')
+} else if (favourite === 7) {
+    console.log('7 is also a good number!')
+    
+} else if (favourite === 9){
+    console.log('9 is also Cool number!')
+} else {
+    console.log('Number is a not 23 or 7 or 9')
+}
+
+if (favourite !== 23) console.log('Why not 23?')
+*/
+
+
+
