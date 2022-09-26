fa5c0f32db2edbe78c0807fa8ea2cddc243a627e
sirhispalis1986@gmail.com <sirhispalis1986@gmail.com>
2022-09-26 08:58:17 +0200

    Update 26/09/22

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -249,7 +249,137 @@ if (favourite === 23) {
 }
 
 if (favourite !== 23) console.log('Why not 23?')
+
+
+//24. Logical Operators
+const hasDriversLicense = true; // A
+const hasGoodVision = false; // B
+
+console.log(hasDriversLicense & hasGoodVision); // false
+console.log(hasDriversLicense || hasGoodVision); // true
+console.log(!hasDriversLicense); // false
+
+
+// if(hasDriversLicense && hasGoodVision) {
+//     console.log('Sarah is able to drive');
+// } else {
+//     console.log('Someone else should drive...');
+// }
+
+const isTired = true; // C
+console.log(hasDriversLicense || hasGoodVision || isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log('Anna is able to drive!');
+} else {
+    console.log('Someone else should drive...');
+}
+
+
+// Coding Chagenge #3
+
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91+ 110) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if ( scoreDolphins > scoreKoalas) {
+//     console.log('Dolhpins win the trophy!!!')
+// } else if  (scoreDolphins < scoreKoalas){
+//     console.log('Koalas win the Trophy!!')
+// } else if (scoreDolphins === scoreKoalas) {
+//     console.log('Both win the trophy!')
+// }
+
+// BONUS 1
+const scoreDolphins = (97 + 112 + 81) / 3;
+const scoreKoalas = (109 + 95 + 86) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if ( scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log('Dolhpins win the trophy!!!')
+} else if  (scoreDolphins < scoreKoalas && scoreKoalas >= 100){
+    console.log('Koalas win the Trophy!!')
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+    console.log('Both win the trophy!')
+} else {
+    console.log('No one wins the trophy');
+}
+
+
+// 26. The Switch statement
+const day = 'monday'; // Cambiar aqui el dia de la semana que prefieras para sacarlo en consola.
+
+switch (day) {
+    case 'monday':
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos')
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples')
+        break;
+    case 'friday':
+        console.log('Record videos')
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D')
+        break;
+    default:
+        console.log('Not a valid day')
+}
+
+if (day === monday) {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === tuesday) {
+    console.log('Prepare theory videos')
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples')
+} else if (day === 'friday') {
+    console.log('Record videos')
+} else if (day === 'saturday' || day === "sunday") {
+    console.log('Enjoy the weekend :D')
+} else {
+    console.log('Not valid Day!')
+}
+
+
+
+// 27. Statements and Expresions
+3 + 4
+1991
+true && false && !false
+
+if(23 > 10) {
+    const str = '23 is higher';
+}
+
+const me = 'Juan'
+console.log(`I am ${2037 - 1991} years old ${me}`);
+
+
+// 28. Conditional Operator
+const age = 23;
+// age >= 18 ? console.log('I like to drink Wine') : console.log('I like to drink water');
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink)
+
+let drink2;
+if(age >= 18) {
+    drink2 = 'wine';
+}else {
+    drink2 = 'water';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
 */
 
-
-
+// Coding challenge #4
+const bill = 275;
+const tip = bill <= 300 && bill >= 50 ? bill *  0.15 : bill * 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
