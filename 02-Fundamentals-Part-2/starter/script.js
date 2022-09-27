a971a9aca51dca813c3922f39715ae6286bde187
sirhispalis1986@gmail.com <sirhispalis1986@gmail.com>
2022-09-27 11:55:07 +0200

    Update 27.09.22

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -71,4 +71,108 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 console.log(yearsUntilRetirement(1986, 'Juan Carlos'));
 console.log(yearsUntilRetirement(1986, 'Anna'));
 
+
+
+// 36. Functions Calling other Functions
+const cutPieces = function (fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutPieces(apples);
+    const orangePieces = cutPieces(oranges);
+
+    const juice = `Juice with ${applePieces} piece of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+console.log(fruitProcessor(2, 3));
+
+
+// 37. Reviewing Functions
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function(birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years.`)
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired!!`)
+        return -1;
+        
+    }
+}
+console.log(yearsUntilRetirement(1986, 'Juan Carlos'));
+
+
+// .38 Coding Challenge #1
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5)); // 4
+
+// TEST 1
+const scoreDolphins =  calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function(avgDolphins, avgKoalas) {
+    if ( avgDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphins win (${avgDolphins}) vs. (${avgKoalas})`);
+    } else if (avgKoalas >= 2 * avgDolphins){
+        console.log(`Koalas win (${avgKoalas}) vs. (${avgDolphins})`);
+    } else {
+        console.log('No team wins...')
+    }
+}
+checkWinner( scoreDolphins, scoreKoalas);
+checkWinner(444, 222);
+
+// TEST 2
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins,scoreKoalas);
+checkWinner(scoreDolphins,scoreKoalas);
 */
+
+// 39. Introduction to Arrays
+const friend1 = 'Juan Carlos'
+const friend2 = 'Anna'
+const friend3 = 'Leo'
+
+const friends = ['Juan Carlos', 'Anna', 'Leo'];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020); // number
+
+console.log(friends[0]) // Primer elemento de Array
+console.log(friends[2])
+
+console.log(friends.length); // te dice cuantos elementos hay en el Array
+console.log(friends[friends.length - 1]) // Leo
+friends[2] = "Rosa" // cambia el nombre del antiguo nombre en este caso Leo
+console.log(friends);
+
+const firstName = "Juanka"
+const Juanka = [firstName, 'Royano', 2037 - 1986, 'programmer', friends ];
+console.log(Juanka);
+console.log(Juanka.length);
+
+// Exercise
+const calcAge = function(birthYear) {
+    return 2037 - birthYear;
+}
+
+const yearsAgain = [1990, 1967, 2002, 2010, 2018];
+const age1 = calcAge(yearsAgain[0]);
+const age2 = calcAge(yearsAgain[1]);
+const age3 = calcAge(yearsAgain[yearsAgain.length - 1]);
+
+console.log(age1, age2, age3)
+
+const ages = [calcAge(yearsAgain[0]), calcAge(yearsAgain[1]), calcAge(yearsAgain[yearsAgain.length - 1])];
+console.log(ages)
