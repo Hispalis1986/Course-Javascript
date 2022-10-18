6f17605866fdeea9393d6687dc55f78b791b143d
Hispalis1986 <sirhispalis1986@gmail.com>
2022-10-19 01:00:33 +0200

    Last update strings

--- 09-Data-Structures-Operators/starter/strings.js
+++ 09-Data-Structures-Operators/starter/strings.js
@@ -12,3 +12,21 @@ console.log(airline.indexOf('r')); // position 6
 console.log(airline.lastIndexOf('r')); // position 10
 console.log(airline.indexOf('Portugal')); // position 8
 console.log(airline.slice(4)); // Air Portugal -- Porque empieza la primera en la cuarta posición.
+console.log(airline.slice(0, airline.indexOf(' '))); // TAP
+console.log(airline.slice(airline.lastIndexOf(' ') + 1)); // Portugal
+console.log(airline.slice(-2)); // Counting from the end // al
+console.log(airline.slice(1, -1)); // Delete the first and the last lyric, letter
+
+const checkMiddleSeat = function (seat) {
+  // B and E are in the middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s == 'E') console.log('You got the middle seat');
+  else console.log('You got lucky');
+};
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('Juan Carlos')); // [String: 'Juan Carlos']
+console.log(typeof new String('Juan Carlos')); // object
+console.log(typeof new String('Juan Carlos').slice(1)); // string
